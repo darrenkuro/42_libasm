893c2f3dbc0349bfcb45c13b943f838a1fd94c2d
dlu <odon5ht@gmail.com>
2023-11-09 01:06:49 +0100

    Complete ft_write for x86

--- Makefile
+++ Makefile
@@ -1,7 +1,9 @@
 NAME	:=	libasm.a
 
+.PHONY: test
+
 test:
-	nasm -f elf64 src/ft_write.s -o src/ft_write.o
+	nasm -f elf64 src/x86_64/ft_write.s -o src/ft_write.o
 	ar rcs $(NAME) src/ft_write.o
 
 mac:
--- src/x86_64/ft_write.s
+++ src/x86_64/ft_write.s
@@ -1,7 +1,17 @@
-.section .text
-.global _ft_write
+section .text
+global	ft_write
+extern	__errno_location
 
-_ft_write:
-	mov eax, 60
-	mov ebx, 0
+ft_write:			; rdi, rsi, rdx for parameters
+	mov		rax, 1	; rax - 64bits, eax - 32, using eax also works, 1 - write syscall
 	syscall
+	js		errno	; check sign flag (SF) which is assigned by syscall if it's neg
+	ret
+
+errno:
+	neg		rax					; get the positive errno, how it is defined
+	push	rax					; push & pop are stack ops for temp data, do not use register, implicitly QWORD
+	call	__errno_location	; external func to get errno addr to rax
+	pop		QWORD [rax]			; pop the QWORD (8 bytes) to where rax is pointing to
+	mov		rax, -1				; reset to -1
+	ret
